4fc825e6248686cb6958e5e399fb2b664648f540
A. Y. Firdaus <amyar32@gmail.com>
2021-04-15 22:16:31 +0700

    menambahkan syntax binding classes

--- 1-starter/2-app/4-basic-styling/app.js
+++ 1-starter/2-app/4-basic-styling/app.js
@@ -1,7 +1,19 @@
 const app = Vue.createApp({
-  data() {
-    return {};
-  },
+    data() {
+        return {
+            isActive: false,
+        };
+    },
+    computed: {
+        mainButton() {
+            return { "is-loading": this.isActive, "is-danger": this.isActive };
+        },
+    },
+    methods: {
+        handleClick() {
+            this.isActive = !this.isActive;
+        },
+    },
 });
 
 app.mount("#my-section");
